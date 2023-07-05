52f7dbdf0519b3685ea6a37ed6748631737f6c18
IdioticMarlin22 <jcrantfo@comcast.net>
2023-07-05 10:48:20 -0400

    Created basic REST connection to server

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form @submit.prevent="getTickerData(stockSymbol)">
+      <label for="ticker-search">Stock Symbol</label>
+      <input type="text" name="ticker-search" v-model="stockSymbol">
+      <button type="submit">Submit</button>
+    </form>
+    <p>{{ stockData }}</p>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue';
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+const stockSymbol = ref('');
+let stockData = ref('');
+
+const getTickerData = (ticker) => {
+  console.log(ticker)
+  
+    axios.post('http://127.0.0.1:8000/stocks/', {
+      ticker: ticker
+    })
+    .then((response) => {
+      console.log(response.data);
+      stockData.value = response.data;
+    })
+    .catch ( (error) => {
+      stockData.value = error.response.data['error']
+    })
+  
 }
 </script>
