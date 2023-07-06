0cdd5c137bae50913085153ce44855351f9c0ad8
IdioticMarlin22 <jcrantfo@comcast.net>
2023-07-05 22:31:09 -0400

    Created nav component, updated readme

---
+++ src/components/navigation-bar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="logo"><span>TSPP</span></div>
+  <div id="search-field-container">
+    <font-awesome-icon :icon="['magnifying-glas']"/>
+    <input type="text" id="search-field">
+  </div>
+  <nav id="mobile-nav">
+  </nav>
+</template>
+
+<script setup>
+
+</script>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// FontAwesome Library
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+library.add( faMagnifyingGlass )
+
 createApp(App).use(router).mount('#app')
